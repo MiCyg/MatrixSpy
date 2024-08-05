ec1ad2748cd329352a190d098c96a1618413ead6
miCyg <68618203+MiCyg@users.noreply.github.com>
2024-08-05 23:49:30 +0200

    change names

--- app.py
+++ app.py
@@ -14,18 +14,22 @@ class App:
 	def __init__(self):
 		self.root = tk.Tk()
 		self.root.resizable(False, False)
-		self.button = tk.Button(text="▶️", font=("Arial", 120), command=self.click_handler)
-		self.button.pack()
+
+		self.frame = tk.Label(text="Speaker Recognition")
+		self.frame.pack()
+
+		self.recordButton = tk.Button(text="▶️", font=("Arial", 60), command=self.click_handler)
+		self.recordButton.pack()
 
 		self.guiQueue = Queue(maxsize=5)
 		self.srCore = SpeakerRecognition(16000, 1024*4)
 
 	def click_handler(self):
 		if self.srCore.is_running():
-			self.button.config(fg="Red")
+			self.recordButton.config(fg="Red")
 			self.srCore.stop()
 		else:
-			self.button.config(fg="Black")
+			self.recordButton.config(fg="Black")
 			self.srCore.start(self.guiQueue)
 
 
